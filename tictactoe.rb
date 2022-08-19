9431e21ea90ed9a9adcc7a500270e0d8ea165308
Betsue Weldemariam <btsegay0@gmail.com>
2022-08-19 22:01:41 +0300

    added a drawing of board function

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,9 +1,50 @@
 class TicTacToe
-
+    
     def initialize
-        @board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+        @board = [1,2,3,4,5,6,7,8,9]
+        @count = 1
+        @turn = 'O'
     end
 
-    
+    winning_move = [
+        [0,1,2], [3,4,5], [6,7,8],
+        [0,3,6], [1,4,7], [2,5,8],
+        [0,4,8], [2,4,6]
+    ]
+
+    def change_turn()
+        @count.even?() ? @turn = 'O' : @turn = 'X'
+        @count += 1
+    end
+
+    def move
+        puts "It's #{@turn}'s turn"
+        input = gets.to_i
+    end
+
+    def insert_move (move)
+        @board[move]
+    end
+
+    def input_to_index (move)
+        index = move - 1
+    end
+
+    def valid_move? (move)
+        @board[move].instance_of?(Integer) && input_to_index.between(0,8)
+    end
+
+    def draw_board
+        puts " "
+        puts "|  #{@board[0]}  |  #{@board[1]}  |  #{@board[2]}  |"
+        puts "-------------------"
+        puts "|  #{@board[3]}  |  #{@board[4]}  |  #{@board[5]}  |"
+        puts "-------------------"
+        puts "|  #{@board[6]}  |  #{@board[7]}  |  #{@board[8]}  |"
+        puts " "
+    end
 
 end
+
+test = TicTacToe.new
+test.draw_board
