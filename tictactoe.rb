dec62197b37a4223328eb71a13cc71ef06eb8b44
Betsue Weldemariam <btsegay0@gmail.com>
2022-08-22 13:04:19 +0300

    valid move function needs a fix

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,10 +1,26 @@
+require 'pry-byebug'
 class TicTacToe
     
     def initialize
-        @board = [1,2,3,4,5,6,7,8,9]
+        @board = ['O',2,3,4,5,6,7,8,9]
         @count = 1
         @turn = 'O'
         @game_ended = false
+        play
+    end
+
+    def play
+        until @game_ended
+            draw_board()
+            input = move()
+            index = input_to_index(input)
+            valid_move?(input)
+            insert_move(index)
+            draw_board()
+            won?()
+            tie?()
+            change_turn()
+        end
     end
 
     @@winning_moves = [
@@ -13,7 +29,7 @@ class TicTacToe
         [0,4,8], [2,4,6]
     ]
 
-    def change_turn()
+    def change_turn
         @count.even?() ? @turn = 'O' : @turn = 'X'
         @count += 1
     end
@@ -31,12 +47,16 @@ class TicTacToe
         index = move - 1
     end
 
-    def position_taken? (move)
-        !@board[move].instance_of?(Integer)
+    def position_taken? (index)
+        !@board[index].instance_of?(Integer)
     end
 
     def valid_move? (move)
-        position_taken?(move) && input_to_index(move).between(0,8)
+        until position_taken?(input_to_index(move)) || input_to_index(move).between?(0,8)
+            puts "Wrong input. please enter a valid input"
+            draw_board()
+            move()
+        end
     end
 
     def draw_board
@@ -53,14 +73,21 @@ class TicTacToe
         winning_move = @@winning_moves.any? do |move|
             position_taken?(move[0]) && @board[move[0]] == @board[move[1]] && @board[move[0]] == @board[move[2]]  
         end
-        winning_move ? game_ended = true : game_ended = false
+        if winning_move 
+             @game_ended = true 
+             puts "Congratulations! #{@turn} won the game."
+        else
+            @game_ended = false
+        end
     end
 
     def tie?
-        if !@board.all?(Integer)
+        if @board.all?(Integer)
             @game_ended = true
             puts "Game ended in a tie."
         end
     end
 
 end
+
+game = TicTacToe.new
